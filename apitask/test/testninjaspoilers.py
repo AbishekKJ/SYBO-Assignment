030d548f19c2b99893c35320cda70e558a6cf130
AbishekKJ <jayabishek91@gmail.com>
2021-07-10 00:04:45 +0530

    unit test changes

--- apitask/test/testninjaspoilers.py
+++ apitask/test/testninjaspoilers.py
@@ -17,7 +17,15 @@ class TestNinjaSpoilerServices(unittest.TestCase):
         assert isinstance(resp, dict)
 
     def test_save_game_state(self):
-        pass
+        save_game_state_obj = NinjaSpoilersUserGames("aec3d5c9-06f3-4c18-b723-4e933de58a5b")
+        score = 395
+        for i in range(1, 100):
+            score = score + 1
+            game_data = {
+                "gamesPlayed": i,
+                "score": score
+            }
+            save_game_state_obj.save_game_state(game_data)
 
     def test_load_game_state(self):
         pass
