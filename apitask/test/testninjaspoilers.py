e0ebf98555f368dc1d54bd42150536415328559f
AbishekKJ <jayabishek91@gmail.com>
2021-07-10 20:04:29 +0530

    unit test changes

--- apitask/test/testninjaspoilers.py
+++ apitask/test/testninjaspoilers.py
@@ -30,23 +30,24 @@ class TestNinjaSpoilerServices(unittest.TestCase):
             save_game_state_obj.save_game_state(game_data)
 
     def test_load_game_state(self):
-        load_game_state_obj = NinjaSpoilersUserGames("aec3d5c9-06f3-4c18-b723-4e933de58a5b")
+        load_game_state_obj = NinjaSpoilersUserGames("5dc50172-e18b-11eb-84da-3cf011dfc659")
         resp = load_game_state_obj.load_game_state()
         assert isinstance(resp, dict)
 
     def test_update_friends(self):
-        update_obj = NinjaSpoilersUserFriends("aec3d5c9-06f3-4c18-b723-4e933de58a5b")
+        update_obj = NinjaSpoilersUserFriends("5dc50172-e18b-11eb-84da-3cf011dfc659")
         friends_data = {
-            "friends": ["31896d4e-e179-11eb-84da-3cf011dfc659",
-                        "35dc2198-e179-11eb-84da-3cf011dfc659",
-                        "3b068aa0-e179-11eb-84da-3cf011dfc659",
-                        "3dc9623a-e179-11eb-84da-3cf011dfc659"]
+            "friends": ["5f3b4a0c-e18b-11eb-84da-3cf011dfc659",
+                        "5f3b4a0d-e18b-11eb-84da-3cf011dfc659",
+                        "5f3b4a0e-e18b-11eb-84da-3cf011dfc659",
+                        "5fe9ad40-e18b-11eb-84da-3cf011dfc659",
+                        "5fe9ad41-e18b-11eb-84da-3cf011dfc659"]
         }
         resp = update_obj.update_friends(friends_data)
         assert isinstance(resp, dict)
 
     def test_get_friends(self):
-        get_obj = NinjaSpoilersUserFriends("aec3d5c9-06f3-4c18-b723-4e933de58a5b")
+        get_obj = NinjaSpoilersUserFriends("5dc50172-e18b-11eb-84da-3cf011dfc659")
         resp = get_obj.get_friends()
         assert isinstance(resp, dict)
 
