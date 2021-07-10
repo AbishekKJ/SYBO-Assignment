443b0ec5727464f8c00bc628c47df1fb82ba1fe5
AbishekKJ <jayabishek91@gmail.com>
2021-07-10 18:45:00 +0530

    user fiends api test cases

--- apitask/test/testninjaspoilers.py
+++ apitask/test/testninjaspoilers.py
@@ -13,7 +13,7 @@ class TestNinjaSpoilerServices(unittest.TestCase):
 
     def test_create_user(self):
         user_obj = NinjaSpoilersUsers()
-        resp = user_obj.create_user("Abishek")
+        resp = user_obj.create_user("Abishek S")
         assert isinstance(resp, dict)
 
     def test_save_game_state(self):
@@ -30,6 +30,22 @@ class TestNinjaSpoilerServices(unittest.TestCase):
     def test_load_game_state(self):
         load_game_state_obj = NinjaSpoilersUserGames("aec3d5c9-06f3-4c18-b723-4e933de58a5b")
         resp = load_game_state_obj.load_game_state()
+        assert isinstance(resp, dict)
+
+    def test_update_friends(self):
+        update_obj = NinjaSpoilersUserFriends("aec3d5c9-06f3-4c18-b723-4e933de58a5b")
+        friends_data = {
+            "friends": ["31896d4e-e179-11eb-84da-3cf011dfc659",
+                        "35dc2198-e179-11eb-84da-3cf011dfc659",
+                        "3b068aa0-e179-11eb-84da-3cf011dfc659",
+                        "3dc9623a-e179-11eb-84da-3cf011dfc659"]
+        }
+        resp = update_obj.update_friends(friends_data)
+        assert isinstance(resp, dict)
+
+    def test_get_friends(self):
+        get_obj = NinjaSpoilersUserFriends("aec3d5c9-06f3-4c18-b723-4e933de58a5b")
+        resp = get_obj.get_friends()
         assert isinstance(resp, dict)
 
 
