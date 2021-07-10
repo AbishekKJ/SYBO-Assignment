1194405146be349639a5218c35ba9cd440663717
AbishekKJ <jayabishek91@gmail.com>
2021-07-10 23:07:45 +0530

    lambda handler changes

--- apitask/handler.py
+++ apitask/handler.py
@@ -4,17 +4,11 @@ Author: Abishek KJ
 Date: 09-Jul-2021
 """
 
-import json
+from apitask.services import NinjaSpoilersManager
 
 
-def get_ninja_user_services(event, context):
-    resource_path = event.get("resource", "")
-    resource_map = {}
-    response = {"headers": {
-        'Access-Control-Allow-Origin': '*'
-    }}
-    if resource_path:
-        pass
-    response["statusCode"] = 404
-    response["body"] = "Data not found"
+def get_ninja_spoilers_user_services(event, context):
+    manager_obj = NinjaSpoilersManager(event, context)
+    response = manager_obj.run()
     return response
+
