b627de53a50834087d1a6b33fd4e476f29fca879
AbishekKJ <jayabishek91@gmail.com>
2021-07-11 15:41:58 +0530

    relative import changes

--- apitask/services/__init__.py
+++ apitask/services/__init__.py
@@ -4,8 +4,8 @@ Author: Abishek KJ
 Date: 09-Jul-2021
 """
 
-from apitask.services.ninjaspoilersbase import NinjaSpoilers
-from apitask.services.ninjaspoilersusergames import NinjaSpoilersUserGames
-from apitask.services.ninjaspoilersusers import NinjaSpoilersUsers
-from apitask.services.ninjaspoilersuserfriends import NinjaSpoilersUserFriends
-from apitask.services.ninjaspoilersmanager import NinjaSpoilersManager
+from .ninjaspoilersbase import NinjaSpoilers
+from .ninjaspoilersusergames import NinjaSpoilersUserGames
+from .ninjaspoilersusers import NinjaSpoilersUsers
+from .ninjaspoilersuserfriends import NinjaSpoilersUserFriends
+from .ninjaspoilersmanager import NinjaSpoilersManager
