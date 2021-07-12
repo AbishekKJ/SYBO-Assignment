0c7e135a343bce9ac486cee843fb271c60309bb3
AbishekKJ <jayabishek91@gmail.com>
2021-07-12 17:02:17 +0530

    PEP changes

--- apitask/constant.py
+++ apitask/constant.py
@@ -12,6 +12,7 @@ class Resources(Enum):
     Resource path values
     """
     CREATE_USER = "/ninjaSpoilers/api/v1/user"
+    GET_ALL_USERS = "/ninjaSpoilers/api/v1/users"
     LOAD_SAVE_GAME_STATE = "/ninjaSpoilers/api/v1/user/{userId}/state"
     UPDATE_GET_FRIENDS = "/ninjaSpoilers/api/v1/user/{userId}/friends"
 
